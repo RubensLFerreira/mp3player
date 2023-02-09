af66ccb26592371c89ee69c1ebebce43c874802f
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-09 17:32:49 -0300

    testando

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Player from "./components/Player";
-import Description from "./components/Description";
+// import Player from "./components/Player";
+// import Description from "./components/Description";
+import Testando from './components/Testando'
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Player />} />
-        <Route path="/description" element={<Description />} />
+        {/* <Route path="/" element={<Player />} /> */}
+        {/* <Route path="/description" element={<Description />} /> */}
+        <Route path='/' element={<Testando />} />
       </Routes>
     </div>
   );
